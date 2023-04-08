36922421519d1c40c81e2c7b4c1673eeaf9cd6c7
alionachernenko <108655520+alionachernenko@users.noreply.github.com>
2023-04-08 19:43:51 +0000

    Deploying to gh-pages from @ alionachernenko/auniverse@7accc8a0804eb6a5536e2262e37dbb03bb250ef7 🚀

--- static/js/199.7dd1a199.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkauniverse=self.webpackChunkauniverse||[]).push([[199],{8199:function(e,n,s){s.r(n);var r=s(2791),u=s(5205),i=s(184),a=(0,r.lazy)((function(){return Promise.resolve().then(s.bind(s,4366))}));n.default=function(){return(0,i.jsxs)("div",{children:[(0,i.jsx)(u.VM,{}),(0,i.jsx)(r.Suspense,{fallback:(0,i.jsx)(u.h2,{}),children:(0,i.jsx)(a,{})})]})}}}]);
-//# sourceMappingURL=199.7dd1a199.chunk.js.map
