502694381934c41b006ef65903f576cbc1531ca4
Alyona Chernenko <errorroror75@gmail.com>
2023-04-28 17:52:54 +0300

    add alphabet sorting in filters

--- src/components/SearchFilter/SearchFilter.jsx
+++ src/components/SearchFilter/SearchFilter.jsx
@@ -2,18 +2,29 @@ import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { fetchDevelopers, fetchGenres, fetchPlatforms } from 'utils';
 
-export const SearchFilter = ({ setOrdering, setGenre, setPlatform, setDeveloper }) => {
+const sortFilterValues = values => {
+  return values.sort((a, b) => a.name.localeCompare(b.name));
+};
+
+export const SearchFilter = ({
+  setOrdering,
+  setGenre,
+  setPlatform,
+  setDeveloper,
+}) => {
   const [genres, setGenres] = useState([]);
   const [platforms, setPlatforms] = useState([]);
   const [developers, setDevelopers] = useState([]);
 
   useEffect(() => {
-    Promise.all([fetchGenres(), fetchPlatforms(), fetchDevelopers()]).then(res => {
-      const [genresRes, platformsRes, developersRes] = res;
-      setGenres(genresRes.data.results);
-      setPlatforms(platformsRes.data.results);
-      setDevelopers(developersRes.data.results)
-    });
+    Promise.all([fetchGenres(), fetchPlatforms(), fetchDevelopers()]).then(
+      res => {
+        const [genresRes, platformsRes, developersRes] = res;
+        setGenres(sortFilterValues(genresRes.data.results));
+        setPlatforms(sortFilterValues(platformsRes.data.results));
+        setDevelopers(sortFilterValues(developersRes.data.results));
+      }
+    );
   }, []);
   return (
     <Filters>
@@ -28,12 +39,12 @@ export const SearchFilter = ({ setOrdering, setGenre, setPlatform, setDeveloper 
         >
           <option>None</option>
           <option value="added">Added</option>
-          <option value="released">Released</option>
-          <option value="name">Name</option>
           <option value="created">Created</option>
-          <option value="updated">Updated</option>
-          <option value="rating">Rating</option>
+          <option value="released">Released</option>
           <option value="metacritic">Metacritic</option>
+          <option value="name">Name</option>
+          <option value="rating">Rating</option>
+          <option value="updated">Updated</option>
         </FilterOptions>
       </Filter>
       <Filter>
@@ -47,12 +58,14 @@ export const SearchFilter = ({ setOrdering, setGenre, setPlatform, setDeveloper 
         >
           <option>All</option>
           {genres.map(genre => (
-            <option key={genre.id} value={genre.slug}>{genre.name}</option>
+            <option key={genre.id} value={genre.slug}>
+              {genre.name}
+            </option>
           ))}
         </FilterOptions>
       </Filter>
       <Filter>
-      <Label htmlFor="platform_select">Platform:</Label>
+        <Label htmlFor="platform_select">Platform:</Label>
         <FilterOptions
           name="platform"
           id="platform_select"
@@ -62,12 +75,14 @@ export const SearchFilter = ({ setOrdering, setGenre, setPlatform, setDeveloper 
         >
           <option>All</option>
           {platforms.map(platform => (
-            <option key={platform.id} value={platform.id}>{platform.name}</option>
+            <option key={platform.id} value={platform.id}>
+              {platform.name}
+            </option>
           ))}
         </FilterOptions>
       </Filter>
       <Filter>
-      <Label htmlFor="developer_select">Developer:</Label>
+        <Label htmlFor="developer_select">Developer:</Label>
         <FilterOptions
           name="developer"
           id="developer_select"
@@ -77,7 +92,9 @@ export const SearchFilter = ({ setOrdering, setGenre, setPlatform, setDeveloper 
         >
           <option>All</option>
           {developers.map(developer => (
-            <option key={developer.id} value={developer.slug}>{developer.name}</option>
+            <option key={developer.id} value={developer.slug}>
+              {developer.name}
+            </option>
           ))}
         </FilterOptions>
       </Filter>
